61fd69b522af7acd2430cad03335051251a0b290
Barakat Abweh <barakat.abweh.96@gmail.com>
2019-12-17 16:10:48 +0200

    Update extend-ulimit-open-files.sh

--- extend-ulimit-open-files.sh
+++ extend-ulimit-open-files.sh
@@ -4,3 +4,7 @@ cp /etc/systemd/user.conf /etc/systemd/user.conf.bckup
 cp /etc/systemd/system.conf /etc/systemd/system.conf.bckup
 sed -i 's/^#DefaultLimitNOFILE=/DefaultLimitNOFILE=65535/' /etc/systemd/user.conf
 sed -i 's/^#DefaultLimitNOFILE=/DefaultLimitNOFILE=65535/' /etc/systemd/system.conf
+echo "*               soft    nproc           65535" >> /etc/security/limits.conf
+echo "*               hard    nproc           65535" >> /etc/security/limits.conf
+echo "*               soft    nofile          65535" >> /etc/security/limits.conf
+echo "*               hard    nofile          65535" >> /etc/security/limits.conf
