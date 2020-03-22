7ae3f6630f2a2b07f0c3827f7dcc1d9b7d9021a1
Barakat Abweh <barakat.abweh.96@gmail.com>
2020-03-22 19:08:41 +0200

    Update extend-ulimit-open-files.sh

--- extend-ulimit-open-files.sh
+++ extend-ulimit-open-files.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-echo "fs.file-max=1000000" >/etc/sysctl.conf
+echo "fs.file-max=65535" >/etc/sysctl.conf
 cp /etc/systemd/user.conf /etc/systemd/user.conf.bckup
 cp /etc/systemd/system.conf /etc/systemd/system.conf.bckup
 sed -i 's/^#DefaultLimitNOFILE=/DefaultLimitNOFILE=65535/' /etc/systemd/user.conf
